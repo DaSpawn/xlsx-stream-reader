7075d5c341aa7d54511a271419ce5ae487dd63a1
giovanni <giovanni.lela@link-me.it>
2017-11-30 21:24:04 +0100

    added basic tests
    fix test expectation
    
    restore tabs

--- test/test.js
+++ test/test.js
@@ -3,54 +3,25 @@ const fs = require('fs');
 const assert = require('assert');
 const path = require('path');
 
-var workBookReader = new XlsxStreamReader();
-workBookReader.on('error', function (error) {
-    throw(error);
-});
-workBookReader.on('sharedStrings', function () {
-    // do not need to do anything with these, 
-    // cached and used when processing worksheets
-    console.log(workBookReader.workBookSharedStrings);
-});
-
-workBookReader.on('styles', function () {
-    // do not need to do anything with these
-    // but not currently handled in any other way
-    console.log(workBookReader.workBookStyles);
-});
-
-workBookReader.on('worksheet', function (workSheetReader) {
-    if (workSheetReader.id > 1){
-        // we only want first sheet
-        workSheetReader.skip();
-        return; 
-    }
-    // print worksheet name
-
-    // if we do not listen for rows we will only get end event
-    // and have infor about the sheet like row count
-    workSheetReader.on('row', function (row) {
-        if (row.attributes.r == 1){
-            // do something with row 1 like save as column names
-        }else{
-            // second param to forEach colNum is very important as
-            // null columns are not defined in the array, ie sparse array
-            row.values.forEach(function(rowVal, colNum){
-                // do something with row values
+describe('The xslx stream parser', function() {
+    it('parses large files', function(done){
+        var workBookReader = new XlsxStreamReader();
+        fs.createReadStream(path.join(__dirname, 'big.xlsx')).pipe(workBookReader);
+        workBookReader.on('worksheet', function (workSheetReader) {
+            workSheetReader.on('end', function () {
+                assert(workSheetReader.rowCount === 80000);
+                done();
             });
-        }
+            workSheetReader.process();
+        });
     });
-    workSheetReader.on('end', function () {
-        assert(workSheetReader.rowCount === 80000);
+    it('catches zip format errors', function(done){
+        var workBookReader = new XlsxStreamReader();
+        fs.createReadStream(path.join(__dirname, 'notanxlsx')).pipe(workBookReader);
+        workBookReader.on('error', function (err) {
+            assert(err.message === 'invalid signature: 0x6d612069');
+            done();
+        });
     });
-
-    // call process after registering handlers
-    workSheetReader.process();
 });
 
-workBookReader.on('end', function () {
-  console.log('END')
-    // end of workbook reached
-});
-
-fs.createReadStream(path.join(__dirname, 'notanxlsx')).pipe(workBookReader);
